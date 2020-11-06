422c48af3c72521fb58f44e6ffd2b4eda614c7ae
Casperinjo <casper.karlsson3@elev.ga.ntig.se>
2020-11-06 18:56:52 +0100

    Nu kommer vinsten att spara i en textfil och sedan skrivas ut för att seden totala vinsten.

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -1,26 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace BlackJack
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             //Deklarera det första korten för person och delare
 
             Random randNummer = new Random();
 
-            
-
             bool gameState = true;
             double vinstVärde = 0;
             double slutSumma = 0;
             int wager = 0;
+            string skrivVinst = File.ReadAllText("vinstTot.txt");
+            double skrivVinstTal = double.Parse(skrivVinst);
 
+            Console.WriteLine($"Din totala vinst {skrivVinstTal} tryck \"ENTER\" för att gå vidare");
+            Console.ReadLine();
 
             while (gameState == true)
             {
@@ -28,249 +27,164 @@ namespace BlackJack
                 bool förlust = false;
                 bool blackJack = false;
                 bool push = false;
-                
-
-
-
 
                 Console.Clear();
-                vinstVärde = vinstVärde + (slutSumma - wager);
-                
-                Console.WriteLine($"Din totala vinst {vinstVärde}" );
-                
 
+                Console.WriteLine($"Din totala vinst den här omgången {vinstVärde}");
 
                 Console.WriteLine("Hur mycket pengar vill du satsa? ");
-                 wager = int.Parse(Console.ReadLine());
-
-                
-
-
+                wager = int.Parse(Console.ReadLine());
 
                 int[] personKort = new int[11];
 
-            int[] delarKort = new int[11];
+                int[] delarKort = new int[11];
 
-            personKort[1] = randNummer.Next(2, 11);
+                personKort[1] = randNummer.Next(2, 11);
 
-            personKort[2] = randNummer.Next(2, 11);
+                personKort[2] = randNummer.Next(2, 11);
 
-            delarKort[1] = randNummer.Next(2, 11);
+                delarKort[1] = randNummer.Next(2, 11);
 
-            delarKort[2] = randNummer.Next(2, 11);
+                delarKort[2] = randNummer.Next(2, 11);
 
-            Console.WriteLine($"Dina två kort var {personKort[1]} och {personKort[2]} och din summa är nu {personKort[1] + personKort[2]}," +
-                $" delarens ena kort var {delarKort[1]} och det andra är okänt " +
-                $"\n Vill du fortsätta? Skriv Ja eller Nej");
+                Console.WriteLine($"Dina två kort var {personKort[1]} och {personKort[2]} och din summa är nu {personKort[1] + personKort[2]}," +
+                    $" delarens ena kort var {delarKort[1]} och det andra är okänt " +
+                    $"\n Vill du fortsätta? Skriv Ja eller Nej");
 
-            string fortsättning = Console.ReadLine();
+                string fortsättning = Console.ReadLine();
 
-            int summaPerson = personKort[1] + personKort[2];
-            int summaDelare = delarKort[1] + delarKort[2];
+                int summaPerson = personKort[1] + personKort[2];
+                int summaDelare = delarKort[1] + delarKort[2];
 
+                //Deklaration för resten av personens kort
 
-            
-            
-            //Deklaration för resten av personens kort
-            
-
-           
-
-            for (int i = 0; i < personKort.Length; i++)
-            {
-
-                if (fortsättning == "Ja")
+                for (int i = 0; i < personKort.Length; i++)
                 {
-
-                    personKort[i] = randNummer.Next(2, 11);
-
-
-
-                    if (personKort[i] == 11)
+                    if (fortsättning == "Ja")
                     {
+                        personKort[i] = randNummer.Next(2, 11);
 
-
-                        Console.WriteLine("Du fick ett \"Ess\" välj om det ska vara en 1:a eller en 11:a ");
-                        int essVal = int.Parse(Console.ReadLine());
-                        if (essVal == 11)
+                        if (personKort[i] == 11)
                         {
-                            personKort[i] = 11;
+                            Console.WriteLine("Du fick ett \"Ess\" välj om det ska vara en 1:a eller en 11:a ");
+                            int essVal = int.Parse(Console.ReadLine());
+                            if (essVal == 11)
+                            {
+                                personKort[i] = 11;
+                            }
+                            if (essVal == 1)
+                            {
+                                personKort[i] = 1;
+                            }
                         }
-                        if (essVal == 1)
-                        {
-                            personKort[i] = 1;
-                        }
-
+                        summaPerson = summaPerson + personKort[i];
+                        Console.WriteLine($" {personKort[i]} Din totalsumma är nu {summaPerson} vill du fortsätta? skriv Ja eller Nej");
+                        fortsättning = Console.ReadLine();
                     }
-                    summaPerson = summaPerson + personKort[i];
-                    Console.WriteLine($" {personKort[i]} Din totalsumma är nu {summaPerson} vill du fortsätta? skriv Ja eller Nej");
-                    fortsättning = Console.ReadLine();
-                }
 
-                if (summaPerson > 21)
-                {
-                    break;
-                }
-            }
-
-            //Deklaration för delarens kort ifall personen inte för över 21
-            if ((summaPerson <= 21))
-            {
-
-
-                Console.WriteLine($"Delarens två kort är {delarKort[1]} och {delarKort[2]} och har en summa på {summaDelare}. \n" +
-                $"tryck \"ENTER\" för att se delarens kort");
-                Console.ReadLine();
-
-                for (int i = 2; i < delarKort.Length; i++)
-                {
-                    delarKort[i] = randNummer.Next(2, 11);
-
-                    summaDelare = summaDelare + delarKort[i];
-
-                    Console.WriteLine($" {delarKort[i]} , Summan är nu {summaDelare} ");
-
-                    if (summaDelare > 21 || (summaDelare >= summaPerson && summaDelare <= 21))
+                    if (summaPerson > 21)
                     {
                         break;
                     }
-
                 }
 
-                if (summaDelare > 21 && summaPerson <= 21)
+                //Deklaration för delarens kort ifall personen inte för över 21
+                if ((summaPerson <= 21))
                 {
-                    
-                        if(summaPerson == 21)
-                        {
-                             blackJack = true;
-                             vinst = true;
-                        }
+                    Console.WriteLine($"Delarens två kort är {delarKort[1]} och {delarKort[2]} och har en summa på {summaDelare}. \n" +
+                    $"tryck \"ENTER\" för att se delarens kort");
+                    Console.ReadLine();
 
+                    for (int i = 2; i < delarKort.Length; i++)
+                    {
+                        delarKort[i] = randNummer.Next(2, 11);
+
+                        summaDelare = summaDelare + delarKort[i];
+
+                        Console.WriteLine($" {delarKort[i]} , Summan är nu {summaDelare} ");
+
+                        if (summaDelare > 21 || (summaDelare >= summaPerson && summaDelare <= 21))
+                        {
+                            break;
+                        }
+                    }
+
+                    if (summaDelare > 21 && summaPerson <= 21)
+                    {
+                        if (summaPerson == 21)
+                        {
+                            blackJack = true;
+                            vinst = true;
+                        }
                         else
                         {
                             Console.WriteLine($"Du vann, delare fick {summaDelare} som summa ");
                             vinst = true;
-
                         }
-                }
-                
-
-
-                else if (summaDelare <= 21 || (summaDelare > summaPerson && summaDelare <= 21))
-                {
-                    if(summaPerson == 21)
+                    }
+                    else if (summaDelare <= 21 || (summaDelare > summaPerson && summaDelare <= 21))
+                    {
+                        if (summaPerson == 21)
                         {
                             push = true;
                         }
-                        
-                    else
+                        else
                         {
-                        Console.WriteLine("Tyvärr delaren var närmare 21 och du förlorade");
-                        förlust = true;
+                            Console.WriteLine("Tyvärr delaren var närmare 21 och du förlorade");
+                            förlust = true;
                         }
-
+                    }
                 }
-
-                
-
-            }
-
-
-            else if (summaPerson > 21)
+                else if (summaPerson > 21)
                 {
-                Console.WriteLine("Du fick över 21 och förlorade");
-                 förlust = true;
-
-
+                    Console.WriteLine("Du fick över 21 och förlorade");
+                    förlust = true;
                 }
-
-                
 
                 if (blackJack == true && vinst == true)
                 {
                     slutSumma = wager * 2.5;
                     Console.WriteLine($"Du vann och fick blackjack och fick {slutSumma - wager} tillbaka ");
                 }
-
-
                 else if (vinst == true)
                 {
                     slutSumma = wager * 2;
                     Console.WriteLine($"Du vann och fick {slutSumma - wager} tillbaka ");
                 }
-
                 else if (push == true)
                 {
                     slutSumma = wager;
                     Console.WriteLine($"Du hamnade lika med delaren och fick tillba {slutSumma} ");
                 }
-
-                else if(förlust == true)
+                else if (förlust == true)
                 {
+                    slutSumma = 0;
                     Console.WriteLine($"Du förlorade och fick inget tillbaka ");
                 }
+
+                vinstVärde = vinstVärde + (slutSumma - wager);
 
                 Console.WriteLine("Vill du spela igen Ja/Nej? ");
                 string omstart = Console.ReadLine();
 
-                if(omstart == "Ja")
+                if (omstart == "Ja")
                 {
                     gameState = true;
                 }
-                
-                else if(omstart == "Nej")
+                else if (omstart == "Nej")
                 {
                     gameState = false;
                 }
-
-                
             }
-                
-                
-                
-            
+
+            double vinstTotInt = skrivVinstTal + vinstVärde;
+
+            string vinstTot = Convert.ToString(vinstTotInt);
+            File.WriteAllText("vinstTot.txt", vinstTot);
 
             Console.WriteLine("Spelet avslutas tryck \"ENTER \" för att gå ut ");
 
             Console.ReadLine();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
     }
 }
-
-
-
-                
-                
-
-
-
